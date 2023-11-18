69778377ae84e5318a1061e18b2f8fc816aeef95
Arisha <arishaaa333@gmail.com>
2023-11-18 21:39:25 +0530

    initial commit

---
+++ AirBnb-Clone/src/assets/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "/airbnb-logo.png"
+
+export const Nav = () => {
+  return (
+    <>
+    <nav>
+    <img src={logo} id='logo' alt="logo image" />
+    </nav>
+    </>
+  )
+}
